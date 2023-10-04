9f38c670402e00167217973c82b91e6a7167210e
NAHID HASAN <129680386+nahidAK005@users.noreply.github.com>
2023-10-04 16:31:34 +0600

    Add files via upload

---
+++ style.js
@@ -0,0 +1,84 @@
+// let menu = document.querySelector("#menu-bars");
+// let navbar = document.querySelector(".navbar");
+
+
+//   menu.onclick = () => {
+//    menu.classList.toggle(".fa-x");
+//     navbar.classList.toggle(".active");
+
+
+//   }
+// let x = document.querySelector("#menu-bars");
+// let y = document.querySelector(".navbar");
+// x.addEventListener("click", function(){
+//     x.classList.toggle('fa-x');
+//     y.classList.toggle('active');
+// });
+
+
+//   
+  // function myFunction() {
+  //   var x = document.getElementById("icon");
+  //   if (x.className === "navbar") {
+  //     x.className += " responsive";
+  //   } else {
+  //     x.className = "navbar";
+  //   }
+  // }
+
+
+
+
+// var swiper = new Swiper(".home-slider", {
+//     spaceBetween: 30,
+//     centeredSlides: true,
+//     autoplay: {
+//       delay: 7500,
+//       disableOnInteraction: false,
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+  
+//    loop: true,
+  
+//   });
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // const progressCircle = document.querySelector(".autoplay-progress svg");
+  // const progressContent = document.querySelector(".autoplay-progress span");
+  // var swiper = new swiper(".home-slider", {
+  //   spaceBetween: 30,
+  //   centeredSlides: true,
+  //   autoplay: {
+  //     delay: 2500,
+  //     disableOnInteraction: false
+  //   },
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true
+  //   },
+  //   navigation: {
+  //     nextEl: ".swiper-button-next",
+  //     prevEl: ".swiper-button-prev"
+  //   },
+  //   on: {
+  //     autoplayTimeLeft(s, time, progress) {
+  //       progressCircle.style.setProperty("--progress", 1 - progress);
+  //       progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+  //     }
+  //   }
+  // });
